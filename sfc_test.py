891bebdb73aca386a986b826e1771fb5e54facfe
jonathancutting <jonathan.cutting@gmail.com>
2025-06-09 10:15:33 -0400

    Added new tests to sfc_test.

--- sfc_test.py
+++ sfc_test.py
@@ -4,23 +4,23 @@ import sfc
 class Tests(unittest.TestCase):
     def test_buildCommandDict_blank(self):
         passed = ""
-        expected = {"cmd": "", "opts": [], "args": []}
+        expected = {"cmd": "", "args": []}
         self.assertEqual(sfc.buildCommandDict(passed), expected)
 
     def test_buildCommandDict_only_command(self):
         passed = "planets"
-        expected = {"cmd": "planets", "opts": [], "args": []}
+        expected = {"cmd": "planets", "args": []}
         self.assertEqual(sfc.buildCommandDict(passed), expected)
 
-    # def test_buildCommandDict_one_short_option_no_args(self):
-    #     passed = "fleet -h"
-    #     expected = {"cmd": "fleet", "opts": [{"opt": "h", "args": []}]}
-    #     self.assertEqual(sfc.buildCommandDict(passed), expected)
+    def test_buildCommandDict_one_arg(self):
+        passed = "fleet -h"
+        expected = {"cmd": "fleet", "args": ["-h"]}
+        self.assertEqual(sfc.buildCommandDict(passed), expected)
 
-    # def test_buildCommandDict_one_long_option_no_args(self):
-    #     passed = "fleet --help"
-    #     expected = {"cmd": "fleet", "opts": [{"opt": "help", "args": []}]}
-    #     self.assertEqual(sfc.buildCommandDict(passed), expected)
+    def test_buildCommandDict_multiple_args(self):
+        passed = "fleet --return Molos"
+        expected = {"cmd": "fleet", "args": ["--return", "Molos"]}
+        self.assertEqual(sfc.buildCommandDict(passed), expected)
 
 if __name__ == "__main__":
     unittest.main()
