eec9af0e925d87441aa0a5b80bd7ca74b8767972
jonathancutting <jonathan.cutting@gmail.com>
2025-06-25 11:50:14 -0400

    Increased specificity of imports in sfc_test.py
    I changed the import within sfc_test.py from importing the entire sfc.py
    file to importing only the functions within sfc.py that were actually
    being tested.

--- sfc_test.py
+++ sfc_test.py
@@ -1,26 +1,26 @@
 import unittest
-import sfc
+from sfc import buildCommandDict
 
 class Tests(unittest.TestCase):
     def test_buildCommandDict_blank(self):
         passed = ""
         expected = {"cmd": "", "args": []}
-        self.assertEqual(sfc.buildCommandDict(passed), expected)
+        self.assertEqual(buildCommandDict(passed), expected)
 
     def test_buildCommandDict_only_command(self):
         passed = "planets"
         expected = {"cmd": "planets", "args": []}
-        self.assertEqual(sfc.buildCommandDict(passed), expected)
+        self.assertEqual(buildCommandDict(passed), expected)
 
     def test_buildCommandDict_one_arg(self):
         passed = "fleet -h"
         expected = {"cmd": "fleet", "args": ["-h"]}
-        self.assertEqual(sfc.buildCommandDict(passed), expected)
+        self.assertEqual(buildCommandDict(passed), expected)
 
     def test_buildCommandDict_multiple_args(self):
         passed = "fleet --return Molos"
         expected = {"cmd": "fleet", "args": ["--return", "Molos"]}
-        self.assertEqual(sfc.buildCommandDict(passed), expected)
+        self.assertEqual(buildCommandDict(passed), expected)
 
 if __name__ == "__main__":
     unittest.main()
