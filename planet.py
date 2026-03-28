971b55f7cc9831a1224761d0b2b8dd56d1720cff
jonathancutting <jonathan.cutting@gmail.com>
2026-03-28 11:39:59 -0400

    Switched parseLocation to use data classes, instead of dicts.

--- planet.py
+++ planet.py
@@ -71,38 +71,38 @@ class Planet:
         self.tasks = {}
         self.summary = {}
 
-def parseLocation(loc:str) -> dict:
+def parseLocation(loc:str) -> Location:
     '''
     Parses a location string like "15:450:5m" into a dict with galaxy, system,
     slot, and moon.
 
     :param loc: Location string in format "G:S:P" or "G:S:Pm" (m = moon)
     :type loc:  str
-    :return:    dict with keys "galaxy", "system", "slot", and "moon"
-    :rtype:     dict (str, int|bool)
+    :return:    Location data class object
+    :rtype:     Location
     '''
 
     logger.debug("Planet location passed: %s", loc)
-    location = {"galaxy":0, "system":0, "slot":0, "moon":False}
+    location = Location()
 
     try:
         elems = loc.strip().split(":")  # This will return a list of strings.
         if len(elems) != 3:             # Check that the list has exectly 3 elements.
             raise ValueError(f"Invalid location format \"{loc}\". Expected \"G:S:P\" or \"G:S:Pm\"")
 
-        location["galaxy"] = int(elems[0])
-        location["system"] = int(elems[1])
+        location.galaxy = int(elems[0])
+        location.system = int(elems[1])
 
         if elems[2].endswith("m"):      # Detect if the location designates a moon.
-            location["slot"] = int(elems[2][:-1])
-            location["moon"] = True
+            location.slot = int(elems[2][:-1])
+            location.moon = True
         else:
-            location["slot"] = int(elems[2])
-            location["moon"] = False
+            location.slot = int(elems[2])
+            location.moon = False
         
         # Log successful parse.
-        locStr = f"{location['galaxy']}:{location['system']}:{location['slot']}"
-        if location["moon"]: locStr += "m"
+        locStr = f"{location.galaxy}:{location.system}:{location.slot}"
+        if location.moon: locStr += "m"
         logger.debug("Parsed planet location: %s", locStr)
 
         return location
